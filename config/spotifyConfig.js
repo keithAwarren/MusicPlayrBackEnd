0582c1c9b60a8fc2805659b6f076ca7a36691b87
Keith Warren <128069942+keithAwarren@users.noreply.github.com>
2024-09-19 14:01:56 -0230

    install dotenv

--- config/spotifyConfig.js
+++ config/spotifyConfig.js
@@ -1,7 +1,6 @@
 module.exports = {
-    clientId: '74745c6eaa6c44c9a6fe6ef0e6fa6310',
-    clientSecret: 'f8d03d8aa51649c7a2f26866b9720f1c',
-    redirectUri: 'https://keithawarren.github.io/MusicPlayr/',
+    clientId: 'process.env.SPOTIFY_CLIENT_ID',
+    redirectUri: 'proess.env.SPOTIFY_REDIRECT_URI',
     scopes: [
         "user-library-read",
         "playlist-read-private",
