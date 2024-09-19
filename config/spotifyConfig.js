97c3ed2666a787b0dfb9491cb5c554650660b59a
Keith Warren <128069942+keithAwarren@users.noreply.github.com>
2024-09-19 12:57:57 -0230

    file set up + spotify configuration v1

---
+++ config/spotifyConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    clientId: '74745c6eaa6c44c9a6fe6ef0e6fa6310',
+    clientSecret: 'f8d03d8aa51649c7a2f26866b9720f1c',
+    redirectUri: 'https://keithawarren.github.io/MusicPlayr/',
+    scopes: [
+        "user-library-read",
+        "playlist-read-private",
+        "streaming",
+        "app-remote-control",
+    ],
+};
