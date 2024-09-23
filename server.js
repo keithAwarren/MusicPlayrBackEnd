d9978117bdd12b7d6a0fe933f82c8a8f19dd447a
Keith Warren <128069942+keithAwarren@users.noreply.github.com>
2024-09-23 16:36:44 -0230

    create server

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-require('dotenv').config();
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const port = process.env.PORT;
 
-const express = require('express');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
